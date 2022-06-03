b5dc13d41d6e271612e1428af7bd9c76e3c95065
Hala Grant <hala.grant@flairstech.com>
2022-06-03 18:51:25 +0200

    updates in subtraction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   title = 'SuprNationCalculator';
   expression:any='';
   result:any;
-  calculatorElements=[0,1,2,3,4,5,6,7,8,9,'+', '−', '∗', '/','sin', 'cos', 'tan','del','C','RAND'];
+  calculatorElements=[0,1,2,3,4,5,6,7,8,9,'+', '-', '∗', '/','sin', 'cos', 'tan','del','C','RAND'];
   shouldInsertBraces=false;
 
 
